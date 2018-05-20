87d98f47d65ef93533dfc9aef44f7acad8d0bc8a
Sean <toranos@gmail.com>
2018-05-20 19:44:06 +1000

    Working typed implementation
    Passes unit tests

--- Hare.hs
+++ Hare.hs
@@ -3,17 +3,35 @@ module Hare where
 import Control.Monad
 import Control.Applicative 
 import HareMonad 
+-- import HareMonad (Hare, hare, failure, readCharacter)
 
 data RE :: * -> * where 
   Empty :: RE ()
   Fail :: RE a
-  -- Char 
-  -- Seq
-  -- Choose
-  -- Star
+  Char :: [Char] -> RE Char
+  Seq :: RE a -> RE b -> RE (a, b)
+  Choose :: RE a -> RE a -> RE a -- is this right?
+  Star :: RE a -> RE [a]
   Action :: (a -> b) -> RE a -> RE b
 match :: (Alternative f, Monad f) => RE a -> Hare f a
-match re = error "'match' unimplemented"
+match Empty = pure ()
+match Fail = failure
+match (Char cs) = do
+  x <- readCharacter
+  guard (x `elem` cs)
+  pure x
+match (Seq a b) = do 
+  ra <- match a 
+  rb <- match b  
+  pure (ra, rb)
+match (Choose a b) = match a <|> match b
+match (Star a) =
+      addFront <$> match a <*> match (Star a)
+  <|> pure []
+  where 
+    addFront x xs = x:xs
+  
+
 
 
 matchAnywhere :: (Alternative f, Monad f) => RE a -> Hare f a
